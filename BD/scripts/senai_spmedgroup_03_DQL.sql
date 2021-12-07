c28f04e1b8b5c5c33ba9aab1d51ad106fa85c798
unknown <43717830821>
2021-12-07 11:14:02 -0300

    DQL pronto

--- BD/scripts/senai_spmedgroup_03_DQL.sql
+++ BD/scripts/senai_spmedgroup_03_DQL.sql
@@ -9,6 +9,22 @@ GO
 SELECT Nome_Cliente, data_Nasc, tel_Paciente, RG_Paciente, CPF_Paciente, End_Paciente FROM Clientes
 GO
 
+-- Função para calcular a quantidade de médicos de cada especialidade
+CREATE FUNCTION CalcMedic (@Id int) RETURNS TABLE AS RETURN
+SELECT count (Id_Medico) AS quantMed FROM Medicos WHERE Id_Especialidade = @Id
+GO
+SELECT * FROM CalcMedic(2)
+GO
+
+-- Função para calcular a idade dos pacientes
+CREATE PROCEDURE CalcIdade AS Select Id_Clientes, Nome_Cliente, DATEDIFF(YEAR, (data_Nasc), GETDATE()) from Clientes
+GO
+EXEC CalcIdade
+
+-- Seleciona as consultas de um paciente especifico
+SELECT * FROM Consulta WHERE Id_Clientes = 3
+GO
+
 
 -- Seleciona todos os tipos de usuários
 SELECT * FROM TipoUsuario
@@ -22,14 +38,26 @@ GO
 SELECT * FROM Administrador
 GO
 
+-- Seleciona todos os clientes 
+SELECT * FROM Clientes
+GO
+
+-- Seleciona todas as clínicas
+SELECT * FROM Clinica
+GO
+
+-- Seleciona todas as especialidades
+SELECT * FROM Especialidade
+GO
+
+-- Seleciona todos os médicos
+SELECT * FROM Medicos
+GO
+
+-- Seleciona as situações 
+SELECT * FROM Situacao
+GO
 
 -- Seleciona Todas as Consultas
 SELECT * FROM Consulta
 GO
-
--- Seleciona as consultas de um paciente especifico
-SELECT * FROM Consulta WHERE Id_Clientes = 3
-GO
-
-Select* from Clientes
-go
