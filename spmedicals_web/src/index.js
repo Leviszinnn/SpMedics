8110532b96993c26e601781b0e27c55cc024bf8c
unknown <43717830821>
2021-12-16 11:25:00 -0300

    pushando

--- spmedicals_web/src/index.js
+++ spmedicals_web/src/index.js
@@ -5,11 +5,11 @@ import {
   BrowserRouter as Router,
   Switch,
 } from 'react-router-dom';
-import { parseJwt, usuarioLogado } from './services/auth';
+// import { parseJwt, usuarioLogado } from './services/auth';
 
 
 import './index.css';
-import App from './App';
+// import App from './App';
 
 import Login from './pages/login/login';
 
@@ -29,8 +29,4 @@ ReactDOM.render(
   routing,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
